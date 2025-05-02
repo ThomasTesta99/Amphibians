fb296888f8738ead95949e24f591b49b229089c9
ThomasTesta99 <ttesta99@yahoo.com>
2025-05-02 11:43:52 -0400

    AmphibianApiService

--- app/src/main/java/com/example/amphibians/network/AmphibianPhoto.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibianPhoto.kt
@@ -1,8 +1,12 @@
 package com.example.amphibians.network
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AmphibianPhoto(
     val name: String,
     val type: String,
     val description: String,
-
+    @SerialName(value = "img_src") val imgSrc : String
 )
